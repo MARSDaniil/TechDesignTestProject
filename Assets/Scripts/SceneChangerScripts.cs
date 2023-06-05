e1943ab244a65626de107a390c217e636ab184d6
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-06-05 16:27:19 +0300

    folders, base scene, scene script, background
    The main folders are created, the base scene with scene change buttons is set, the transition script between scenes is added, the background is created on the base scene

---
+++ Assets/Scripts/SceneChangerScripts.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChangerScripts : MonoBehaviour
+{
+    
+   
+
+    public void ChangeSceneByName(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void NextScene()
+    {
+        
+        if (SceneManager.GetActiveScene().buildIndex != SceneManager.sceneCount-1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+            Debug.Log("It was last scene");
+        }
+    }
+    public void PreviousScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        else
+        {
+            Debug.Log("It was first scene");
+            SceneManager.LoadScene(SceneManager.sceneCount - 1);
+        }
+        
+    }
+}
